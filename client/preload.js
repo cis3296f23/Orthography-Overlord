f72d36312978614f4cd6369882f46c42acefee3b
mustafamalik-tu <mustafa.malik@temple.edu>
2023-11-10 11:38:17 -0500

    Merged changes from main

--- client/preload.js
+++ client/preload.js
@@ -1,36 +1,53 @@
 const { ipcRenderer } = require('electron')
 
 class GameManager {
-    constructor(_inputContainer) {
+    constructor(_inputContainer, _audioPlayer, _winCallback) {
         this.inputBoxes = [];
         this.currentInputIndex = 0;
         this.inputContainer = _inputContainer;
+        this.audioPlayer = _audioPlayer;
         this.currentWord = "";
-    }
 
-    hint_constructor() {
-        user_input = this.getEnteredLetters
-        let hint = "";
-        let input_len = user_input.length;
-        for (let i = 0; i < this.currentWord.length - 1; i++) {
-            if (i < input_len && word[i] === user_input[i]) {
-                hint += word[i];
-                continue;
-            }
-            if (hint.length + 1 < this.currentWord.length) {
-                hint += word[i];
-            }
-            break;
+        this.loadedAudio = [];
+        this.wordlist = [];
+
+        this.winCallback = _winCallback;
+
+        this.solvedWords = [];   
+        this.gameIndex = 0;
+    }
+    
+    setupGame = async (wordlist) => {
+        this.wordlist = wordlist;
+        this.loadedAudio = [];
+
+        for(var word of this.wordlist) {
+            var filename = await this.loadAudio(word);
+            this.loadedAudio.push(filename);
         }
-        return hint;
-    }
-      
 
-    resetWord() {
+        this.solvedWords = [];
+        this.gameIndex = 0;
+        this.loadWord();
+    }
+
+    nextWord = () => {
+        if(this.gameIndex < this.wordlist.length-1) {
+            this.gameIndex++;
+            this.loadWord();
+        } else {
+            console.log("YOU WIN!!!!!!!!!!!!!!!!!!")
+            this.winCallback();
+            this.setupGame(["flatulence", "armor"]);
+        }
+    }
+    
+    resetWord = () => {
         this.inputContainer.innerHTML = "";
         this.inputBoxes = [];
         this.currentInputIndex = 0;
         this.currentWord = "";
+        this.clearInput();
     }
 
     clearInput() {
@@ -48,11 +65,14 @@ class GameManager {
         return entered;
     }
 
-    
-    loadWord = (word) => {
-        this.resetWord();
 
+    loadWord = () => {
+        var word = this.wordlist[this.gameIndex];
+        this.audioPlayer.src = this.loadedAudio[this.gameIndex];
+        this.audioPlayer.play();
+        this.resetWord();
         this.currentWord = word;
+
         for(const _ in word) {
             var input = document.createElement('input');
             input.type = "text";
@@ -62,8 +82,6 @@ class GameManager {
             this.inputBoxes.push(input);
         }
     }
-
-
 
     onTypeLetter = (e) => {
         console.log(e);
@@ -91,10 +109,11 @@ class GameManager {
         }
     }
 
-    checkForWin() {
+    checkForWin = () => {
         
         if(this.currentInputIndex == this.inputBoxes.length && this.getEnteredLetters() == this.currentWord) {
-            console.log("YOU WIN!!!");
+            console.log("next word");
+            this.nextWord();
             return true;
         } else if (this.currentInputIndex == this.inputBoxes.length) {
             // clear input maybe?
@@ -103,41 +122,52 @@ class GameManager {
 
          return false;
     }
+
+    loadAudio = async (word) => {
+        return new Promise((resolve, reject) => {
+            ipcRenderer.send("make-dictionary-request", word);
+
+            ipcRenderer.on('dictionary-data-response', (event, filename) => {
+                // sound.src = filename;
+                console.log(filename);
+                resolve(filename);
+            });
+        
+            ipcRenderer.on('dictionary-error-response', (event, error) => {
+                reject(error);
+            });
+        })
+    }
 }
 
-function getAudio(word) {
-    ipcRenderer.send("make-dictionary-request", word);
-}
+// function getAudio(word) {
+//     ipcRenderer.send("make-dictionary-request", word);
+// }
 
 window.addEventListener('DOMContentLoaded', () => {
     const button = document.getElementById('replayButton');
     const sound = document.getElementById('primaryAudio');
+    const alien = document.getElementById('alien');
+
     const inputContainer = document.getElementById('inputContainer');
 
     function replaySound() {
         sound.play();
     }
-
-    // IPC !!!!
-    // MOVE TO SOMEWHERE ELSE!!!!!!
-
-    ipcRenderer.on('dictionary-data-response', (event, filename) => {
-        sound.src = filename;
-
-    });
-
-    ipcRenderer.on('dictionary-error-response', (event, error) => {
-        // do something with error here
-        console.log("RECEIVED ERROR");
-        console.log(error);
-    });
-
-    // ASK MAIN.JS FOR DATA
     
-    const game = new GameManager(inputContainer);
-    getAudio("barnacle");
-    game.loadWord("barnacle");
+
+    function winCallback() {
+        alien.classList.remove("hidden");
+    }
     
+    const game = new GameManager(inputContainer, sound, winCallback);
+    game.setupGame(["barnacle", "python"]);
+    
+
     document.addEventListener("keydown", game.onTypeLetter);
     button.addEventListener('click', replaySound);
 })
+
+
+
+  
