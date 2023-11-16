dbd41ad5f80cc3b562629b76173ab63397f526fe
mustafamalik-tu <mustafa.malik@temple.edu>
2023-11-16 11:56:33 -0500

    Moved answer sound logic to AudioManager

--- client/public/logic/gameLogic.js
+++ client/public/logic/gameLogic.js
@@ -43,9 +43,10 @@ class InputManager {
 
 
 class AudioManager {
-    constructor(_audioPlayer) {
-        this.audioPlayer = _audioPlayer;
+    constructor(_wordSound, _answerSound) {
+        this.wordSound = _wordSound;
         this.loadedAudio = {};
+        this.answerSound = _answerSound;
     }
 
     loadAudio = async (word) => { return window.electronAPI.loadAudioForWord(word) }
@@ -62,8 +63,22 @@ class AudioManager {
     }
 
     setAudioForWord(word) {
-        this.audioPlayer.src = this.loadedAudio[word];
-        this.audioPlayer.play();
+        this.wordSound.src = this.loadedAudio[word];
+        this.wordSound.play();
+    }
+
+    /*
+    This function plays a sound for correct and incorrect answers.
+    Input:
+        - correct: boolean, true if the answer was correct, false otherwise
+    */
+    playAnswerSound(correct) {
+        if(correct) {
+            this.answerSound.src = "./public/sounds/correct_answer.mp3";
+        } else {
+            this.answerSound.src = "./public/sounds/wrong_answer.mp3";
+        }
+        this.answerSound.play();
     }
 }
 
@@ -100,12 +115,10 @@ class HintManager {
 }
 
 class GameManager {
-    constructor(_inputContainer, _audioPlayer, _hintText, _answerSound) {
+    constructor(_inputContainer, _wordSound, _hintText, _answerSound) {
         this.inputManager = new InputManager(_inputContainer);
-        console.log(this.inputManager);
-        this.audioManager = new AudioManager(_audioPlayer);
+        this.audioManager = new AudioManager(_wordSound, _answerSound);
         this.hintManager = new HintManager(_hintText); 
-        this.answerSound = _answerSound;
 
         this.wordAddedToBack = false;
 
@@ -122,8 +135,7 @@ class GameManager {
     }
 
     nextWord() {
-        this.answerSound.src = "./public/sounds/correct_answer.mp3";
-        this.answerSound.play();
+        this.audioManager.playAnswerSound(true);
         if(this.currentWordIndex < this.wordList.length-1) {
             this.currentWordIndex++;
             setTimeout(() => {
@@ -164,8 +176,7 @@ class GameManager {
             this.wordAddedToBack = true;
         }
 
-        this.answerSound.src = "./public/sounds/wrong_answer.mp3";
-        this.answerSound.play();
+        this.audioManager.playAnswerSound(false);
 
         this.hintManager.displayHint(this.wordList[this.currentWordIndex], this.inputManager.getEnteredLetters());
         this.inputManager.clearInput();
@@ -176,17 +187,17 @@ class GameManager {
 
 window.addEventListener('DOMContentLoaded', () => {
     const replayButton = document.getElementById('replayButton');
-    const sound = document.getElementById('primaryAudio');
+    const wordSound = document.getElementById('primaryAudio');
     const hintText = document.getElementById('hintText')
     const inputContainer = document.getElementById('inputContainer');
     const answerSound = document.getElementById('answerSound');
     
-    const game = new GameManager(inputContainer, sound, hintText, answerSound);
+    const game = new GameManager(inputContainer, wordSound, hintText, answerSound);
 
     document.addEventListener("keydown", game.onTypeLetter);
 
     replayButton.addEventListener('click', () => {
-        sound.play();
+        wordSound.play();
         // Remove focus from the button to prevent automatic triggering on Enter
         replayButton.blur();
     });
